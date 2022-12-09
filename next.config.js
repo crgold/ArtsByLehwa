64b6ee8f4f03b4d839918014110ecab3dd43df04
Chris <crgold@asu.edu>
2022-12-09 14:33:47 +0800

    Initial groundwork for image gallery

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+		remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/random/**",
+      },
+    ],
+	},
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import artistPic from '../public/lehwa.webp'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import { Playfair_Display, Montserrat } from '@next/font/google'
+import Head from "next/head";
+import Image from "next/image";
+import artistPic from "../public/lehwa.webp";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { Playfair_Display, Montserrat } from "@next/font/google";
+import { GetStaticProps } from "next";
 
-const playfair = Playfair_Display({ subsets: ['latin']})
-const montserrat = Montserrat({ subsets: ['latin']})
+const playfair = Playfair_Display({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <div className={playfair.className}>
       <Head>
         <title>Arts By Lehwa</title>
         <meta name="description" content="Lehwa Gold's Art Portfolio" />
       </Head>
 
       <header>
-        <nav className={[styles.nav] + " " + [playfair.className]}>
+        <nav className={styles.nav}>
           <h1>Arts By Lehwa</h1>
           <ul>
             <li>
-              <Link href="/#" >
-                  Portfolio
-              </Link>
+              <Link href="/#">Portfolio</Link>
             </li>
             <li>
               <Link href="/#about" className={styles.navMenuMargin}>
@@ -40,12 +39,23 @@ export default function Home() {
       </header>
 
       <header className={styles.header}>
-        <div className={[styles.headerTextContainer] + " " + [montserrat.className]}>
-          <h1>MABUHAY!<br />I&apos;M LEHWA <span className={styles.textGold}>GOLD</span></h1>
-          <h2>painter <span className={styles.textRed}>/</span> digital artist</h2>
+        <div
+          className={
+            [styles.headerTextContainer] + " " + [montserrat.className]
+          }
+        >
+          <h1>
+            MABUHAY!
+            <br />
+            I&apos;M LEHWA <span className={styles.textGold}>GOLD</span>
+          </h1>
+          <h2>
+            painter <span className={styles.textRed}>/</span> digital artist
+          </h2>
         </div>
 
-        <Image className={styles.picture}
+        <Image
+          className={styles.picture}
           src={artistPic}
           alt="The artist Lehwa Gold"
           width={400}
@@ -54,47 +64,70 @@ export default function Home() {
         />
       </header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <section className={styles.portfolioSection}>
+          <div className={styles.portfolioTitle}>
+            <h1>My Works</h1>
+          </div>
+          <div className={styles.imageGrid}>
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/1"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/2"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/3"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/4"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/5"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/6"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/7"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+            <Image
+              className={styles.galleryImg}
+              src="https://source.unsplash.com/random/?city,night/8"
+              alt="The artist Lehwa Gold"
+              width={250}
+              height={250}
+            />
+          </div>
+        </section>
       </main>
 
       <footer className={styles.footer}>
@@ -103,12 +136,29 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const getStaticProps : GetStaticProps = async () => {
+  const results: JSON = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/image`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API + ":" + process.env.CLOUDINARY_SECRET
+        ).toString("base64")}`,
+      },
+    }
+  ).then((r) => r.json());
+  console.log("results", results);
+  return {
+    props: {},
+  };
+};
